4c10dcbcc16870c3e1873d9a6edc700be31cb0bf
Stivet Fulgencio <stivet2184@gmail.com>
2025-10-24 01:07:39 -0500

    This the first commit.

---
+++ src/main/java/org/alpha/omega/student_microservice/StudentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.alpha.omega.student_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMicroserviceApplication.class, args);
+	}
+
+}
